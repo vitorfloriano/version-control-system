8529f995d26157547976d9c70da7c426d4f6f33e
vitorfloriano <vitorflorianoc@gmail.com>
2023-05-31 22:56:06 -0300

    Just practicing a bit.

---
+++ Topics/Processing strings/The lucky ticket/src/Main.kt
@@ -0,0 +1,14 @@
+import javax.xml.stream.events.Characters
+
+fun main() {
+    // write your code here
+    val input = readLine()!!
+    val splitInput = input.toCharArray()
+    val firstHalf = Character.getNumericValue(splitInput[0]) +
+            Character.getNumericValue(splitInput[1]) +
+            Character.getNumericValue(splitInput[2])
+    val secondHalf = Character.getNumericValue(splitInput[3]) +
+            Character.getNumericValue(splitInput[4]) +
+            Character.getNumericValue(splitInput.last())
+    println(if (firstHalf == secondHalf) "Lucky" else "Regular")
+}
