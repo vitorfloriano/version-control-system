d79ccf4f937198a18742edaff4744ff254cec034
vitorfloriano <vitorflorianoc@gmail.com>
2023-06-05 20:47:52 -0300

    Just practicing a bit.

--- Topics/Processing strings/Double characters/src/Main.kt
+++ Topics/Processing strings/Double characters/src/Main.kt
@@ -1,3 +1,6 @@
 fun main() {
-    // write your code here    
+    // write your code here
+    val input = readLine()!!
+    val result = input.map { it.toString() + it.toString() }.joinToString("")
+    println(result)
 }
